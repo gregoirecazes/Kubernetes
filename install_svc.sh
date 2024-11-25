07710918e55364bdbdec64ae169a02a1b9ab89e2
gregoirecazes <gregoire@cazes-pro.fr>
2024-11-25 10:49:06 +0100

    install_svc.sh

---
+++ install_svc.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ROUGE='\033[0;31m'
+BLANC='\033[0m'
+# Menu interactif
+echo ""
+echo -e "${ROUGE}Sur quel serveur voulez vous installer des services ?${BLANC}"
+echo ""
+echo "1) Debian_Serveur (local)"
+echo "2) Debian_Client_01"
+echo "3) Quitter"
+echo ""
+# Lire le choix de l'utilisateur
+read -p "Entrez votre choix : " choix
+
+# Gérer les choix
+case $choix in
+
+    1)
+        #Lancement vers le script bash list_svc.sh
+        bash /projet/bash/local_choix_svc.sh
+        ;;
+    2)
+        #
+        bash /projet/bash/client_choix_svc.sh
+        ;;
+    3)
+        echo "Au revoir !"
+        exit 0
+        ;;
+    *)
+        echo "Choix invalide. Veuillez réessayer."
+        exit 0
+	;;
+
+esac
